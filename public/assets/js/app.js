07ddc5088f660c857bd9ed59dbf561f331683d73
thalida <thalida.c.noel@gmail.com>
2013-11-27 14:13:10 -0500

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,40 +1,40 @@
 //REQUIRE.JS CONFIG
 require.config({
-    baseUrl: 'public/assets/js/libs',
-    paths: {
-    	app: '../app',
-    	tpl: '../app/tpl', //templates
-        
-	jquery: "http://code.jquery.com/jquery-latest",
-	jqueryui: "http://code.jquery.com/ui/1.10.3/jquery-ui.min",
-	mediaqueries: 'css3-mediaqueries',
-	bootstrap: '/public/assets/bootstrap/js/bootstrap.min',
-	bootstrapSelect: '/public/assets/bootstrap/js/bootstrap-select.min',
-    },
-    shim: {
-    	'jqueryui': {
-    		deps: ['jquery']
-    	},
-        'backbone': {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        'underscore': {
-            exports: '_'
-        },
-        'jquery.insertAtCaret': {
-        	deps: ['jquery']
-        },
-        'bootstrap': {
-        	deps: ['jquery']
-        },
-        'bootstrapSelect': {
-        	deps: ['bootstrap']
-        },
-        'app/router':{
-        	deps: ['jqueryui', 'bootstrapSelect', 'jquery.insertAtCaret', 'app/models/ciphers']
-        }
-    }
+	baseUrl: 'public/assets/js/libs',
+	paths: {
+		app: '../app',
+		tpl: '../app/tpl', //templates
+		
+		jquery: "http://code.jquery.com/jquery-latest",
+		jqueryui: "http://code.jquery.com/ui/1.10.3/jquery-ui.min",
+		mediaqueries: 'css3-mediaqueries',
+		bootstrap: '/public/assets/bootstrap/js/bootstrap.min',
+		bootstrapSelect: '/public/assets/bootstrap/js/bootstrap-select.min',
+	},
+	shim: {
+		'jqueryui': {
+			deps: ['jquery']
+		},
+		'backbone': {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone'
+		},
+		'underscore': {
+			exports: '_'
+		},
+		'jquery.insertAtCaret': {
+			deps: ['jquery']
+		},
+		'bootstrap': {
+			deps: ['jquery']
+		},
+		'bootstrapSelect': {
+			deps: ['bootstrap']
+		},
+		'app/router':{
+			deps: ['jqueryui', 'bootstrapSelect', 'jquery.insertAtCaret', 'app/models/ciphers']
+		}
+	}
 });
 
 require(['jquery', 'backbone', 'app/router'], function ($, Backbone, Router) {
