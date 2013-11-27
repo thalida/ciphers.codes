ee51c45f56ffa99ec9b2c79043e96073886dcc17
thalida <thalida.c.noel@gmail.com>
2013-11-27 14:15:19 -0500

    Update router.js

--- public/assets/js/app/router.js
+++ public/assets/js/app/router.js
@@ -2,23 +2,24 @@
 // ROUTER
 ///////
 define(function (require) {
-    "use strict";
-    var $					=	require('jquery'),
-        Backbone			= 	require('backbone'),
-        SiteMainView		=	require('app/views/main'),
+	"use strict";
+	var 
+		$		=	require('jquery'),
+		Backbone	= 	require('backbone'),
+        	SiteMainView	=	require('app/views/main'),
+        	
+        	$body = $('body'),
+        	mainView = new SiteMainView({el: $body}).render(),
+        	$content = $("#content", mainView.el);
         
-        $body = $('body'),
-        mainView = new SiteMainView({el: $body}).render(),
-        $content = $("#content", mainView.el);
-        
-    return Backbone.Router.extend({
-        routes: {
-            "": "home",
-            //"about": "about" TODO: CREATE ABOUT PAGE
-        },
-        home: function () {
-            $('body').scrollTop(0);
-            $('#wrapper').fadeIn(800);
-        }
-    });
+        return Backbone.Router.extend({
+		routes: {
+			"": "home",
+			//"about": "about" TODO: CREATE ABOUT PAGE
+		},
+		home: function () {
+			$('body').scrollTop(0);
+			$('#wrapper').fadeIn(800);
+		}
+	});
 });
