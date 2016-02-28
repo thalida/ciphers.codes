833e963f10a7df0fdb2a3bf48c0533d2334c7720
Thalida Noel <thalida.c.noel@gmail.com>
2016-02-27 22:22:01 -0500

    Select Component: Created cc:select

---
+++ app/components/select/select.component.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    templateUrl: 'components/select/select.html',
+    bindings: {
+        model: '<',
+        options: '=',
+        expression: '@',
+        styles: '@',
+        onChange: '&?'
+    },
+    controller: function(){
+
+    }
+};
